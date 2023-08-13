1224a2f064ea3b9835cc1e163e0b5b6edbd35a2f
miopanda <114249779+miopanda@users.noreply.github.com>
2023-08-13 15:58:11 +0900

    コントローラーの更新

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -1,7 +1,9 @@
 package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,15 +15,20 @@ public class IssueController {
     private final IssueRepository issueRepository;
 
     @GetMapping("/issueForm")
-    public String showIssueForm(@ModelAttribute("issueForm") IssueForm form) {
+    public String showIssueForm(@ModelAttribute("issueForm") IssueForm form){
         return "issueForm";
     }
 
     @PostMapping("/issues")
-    public String createIssue(IssueForm issueForm) {
+    public String createIssue(IssueForm issueForm){
         issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
         return "redirect:/";
     }
 
-
+    @GetMapping
+    public String showIssues(Model model){
+        var issueList = issueRepository.findAll();
+        model.addAttribute("issueList", issueList);
+        return "index";
+    }
 }
