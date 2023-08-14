aba32e3bdf85a9c7b6e2175829dab4db7132ea0d
miopanda <114249779+miopanda@users.noreply.github.com>
2023-08-14 17:55:57 +0900

    コントローラー修正_id取得

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -30,5 +31,12 @@ public class IssueController {
         var issueList = issueRepository.findAll();
         model.addAttribute("issueList", issueList);
         return "index";
+    }
+
+    @GetMapping("/issues/{id}")
+    public String issueDetail(@PathVariable ling id, Model model){
+        var issue = issueRepository.findById();
+        model.addAttribute("issue", issue);
+        return "detail";
     }
 }
